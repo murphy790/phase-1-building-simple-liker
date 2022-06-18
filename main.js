3fe0c31de3f986747ea9a6145590431c3157799a
Alex Mwaura <alexmwaura36@gmail.com>
2022-06-18 09:43:54 +0300

    commit tree af94f935799d4e5c46790bc8816107c9a12b147a

--- main.js
+++ main.js
@@ -4,8 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector('#modal');
+const heart = document.querySelectorAll('.like-glyph')
 
+modal.classList.add('hidden');
 
+for(var i = 0; i < heart.length;i++){
+  heart[i].addEventListener('click',(e)=>{
+    let heartParent= e.target;
+    heartEvent(heartParent);
+    console.log(heartParent);
+})
+}
+function heartEvent(heartParent){
+  mimicServerCall().then(function(response){
+    heartClicked(heartParent);
+    console.log("Success!");
+    console.log(response);
+  }).catch(function(error){
+  modal.classList.remove('hidden');
+    console.log(error);
+  }
+  )
+}
+
+function heartClicked(heartParent) {
+  if(heartParent.innerHTML === EMPTY_HEART) {
+    //add class
+    heartParent.classList.add('activated-heart');
+    heartParent.innerHTML = FULL_HEART;
+    console.log("Success!");
+  }
+  else {
+    heartParent.classList.remove('activated-heart');
+    heartParent.innerHTML = EMPTY_HEART;
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
